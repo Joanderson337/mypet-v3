6715555550e8c5b3d78343e653dc4f4115b05c69
Joanderson Silva <jhonny_040996@hotmail.com>
2023-01-09 14:40:17 -0300

    fix: lint

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlma1C1_Cf27FvRHxN6G2RQsEj36peYkY",
-  authDomain: "mypetv3-6473c.firebaseapp.com",
-  projectId: "mypetv3-6473c",
-  storageBucket: "mypetv3-6473c.appspot.com",
-  messagingSenderId: "314249774779",
-  appId: "1:314249774779:web:2480a470c1b1d3cbe10b9e"
+  apiKey: 'AIzaSyBlma1C1_Cf27FvRHxN6G2RQsEj36peYkY',
+  authDomain: 'mypetv3-6473c.firebaseapp.com',
+  projectId: 'mypetv3-6473c',
+  storageBucket: 'mypetv3-6473c.appspot.com',
+  messagingSenderId: '314249774779',
+  appId: '1:314249774779:web:2480a470c1b1d3cbe10b9e'
 };
 
 
